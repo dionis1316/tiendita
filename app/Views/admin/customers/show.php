b58536331db2393c8099685da4422f45f406a63a
dionis1316 <info@dcsolution.net>
2026-01-27 00:53:46 -0500

    style: boton cancelar con color marca

--- app/Views/admin/customers/show.php
+++ app/Views/admin/customers/show.php
@@ -301,7 +301,7 @@
           </div>
         </div>
         <div class="modal-footer">
-          <button type="button" class="btn btn-outline-secondary" data-bs-dismiss="modal">Cancelar</button>
+          <button type="button" class="btn btn-brand-warn" data-bs-dismiss="modal">Cancelar</button>
           <button type="submit" class="btn btn-brand-primary" id="manualOrderSubmit">Confirmar pedido</button>
         </div>
       </form>
